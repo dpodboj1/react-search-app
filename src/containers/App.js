2ae805c8fb8761d23d7e86da966ee68284064798
dpodboj <damir.podbojec@gmail.com>
2017-11-21 12:42:37 +0100

    Added comments to new code in App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,24 +16,32 @@ export default class App extends React.Component {
   }
 
   async startQuery(query) {
+    //  if the query is not empty make an API req
     if (query !== '') {
+      //  empty array to push the results from APIs
       const results = [];
+      //  get wikipedia api data
       const wiki = await getDataFromApi(`https://en.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch=${query}`);
+      //  map through the data and push them to results array
       wiki.query.search.map(result =>
         results.push({
           src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Wikipedia-logo.png/50px-Wikipedia-logo.png',
           alt: 'Wikipedia Logo',
           title: result.title,
         }));
+      //  get duckduckgo api data
       const duck = await getDataFromApi(`https://api.duckduckgo.com/?q=${query}&format=json`);
+      //  map through the data and push them to results array
       duck.RelatedTopics.map(result =>
         results.push({
           src: 'https://duckduckgo.com/assets/icons/meta/DDG-icon_256x256.png',
           alt: 'DuckDuckGo Logo',
           title: result.Text,
         }));
+      //  update the results array with results from all API
       this.setState({ results });
     } else {
+      //  if the query is empty then reset the results array
       this.setState({ results: [] });
     }
   }
